209c2b2ebf11971bd50c7c3a11380cee03eb69bb
MaedaKouta <maedakoutaa@gmail.com>
2022-02-15 10:13:55 +0900

    Initial Commit

---
+++ MiniApp-InputTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp-InputTableViewCell
+//
+//  Created by 前田航汰 on 2022/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
